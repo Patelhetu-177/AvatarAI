f3b05d83a8bb129dcd8029043593e66625f6c38e
Patelhetu-177 <hetup9432@gmail.com>
2025-12-14 22:33:40 +0530

    resolved issue related slow redirection in sidebaR

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,11 +11,11 @@ import {
   Bot,
   ShieldQuestionIcon,
 } from "lucide-react";
-import { usePathname , useRouter} from "next/navigation";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export const Sidebar = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const routes = [
     {
@@ -68,17 +68,13 @@ export const Sidebar = () => {
     },
   ];
 
-  const onNavigate = (url: string) => {
-    return router.push(url);
-  };
-
   return (
     <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
       <div className="p-3 flex flex-1 justify-center">
         <div className="space-y-2">
           {routes.map((route) => (
-            <div
-              onClick={() => onNavigate(route.href)}
+            <Link
+              href={route.href}
               key={route.href}
               className={cn(
                 "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
@@ -89,7 +85,7 @@ export const Sidebar = () => {
                 <route.icon className="h-5 w-5" />
                 {route.lable}
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
