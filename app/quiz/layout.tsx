b441f7d61e4c0146bf9ac6a5a00853169cde16f3
Patelhetu-177 <hetup9432@gmail.com>
2026-03-27 09:09:03 +0530

    Typescript issue resolved

--- app/quiz/layout.tsx
+++ app/quiz/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navbar";
 import { Sidebar } from "@/components/Sidebar";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function QuizLayout({
@@ -9,20 +10,21 @@ export default async function QuizLayout({
   children: React.ReactNode;
 }) {
   const user = await currentUser();
-  
+
+  const { has } = await auth();
+  const plan = has({ plan: "pro" }) ? "pro" : "free";
+
   if (!user) {
     redirect("/sign-in");
   }
 
   return (
     <div className="h-full">
-      <Navbar />
+      <Navbar plan={plan} />
       <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
         <Sidebar />
       </div>
-      <main className="md:pl-20 pt-16 h-full">
-        {children}
-      </main>
+      <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
 }
